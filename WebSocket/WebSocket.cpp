a88312ea2ff6889d2ff677cea227c83760a19aca
JC Dansereau <jcdan960@gmail.com>
2019-12-24 10:41:35 -0500

    Add WebSocket project.

---
+++ WebSocket/WebSocket.cpp
@@ -0,0 +1,5 @@
+//
+// Created by root on 12/24/19.
+//
+
+#include "WebSocket.h"
---
+++ WebSocket/WebSocket.h
@@ -0,0 +1,14 @@
+//
+// Created by root on 12/24/19.
+//
+
+#ifndef WEBSOCKET_WEBSOCKET_H
+#define WEBSOCKET_WEBSOCKET_H
+
+
+class WebSocket {
+
+};
+
+
+#endif //WEBSOCKET_WEBSOCKET_H
